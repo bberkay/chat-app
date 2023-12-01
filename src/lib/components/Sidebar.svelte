32dfa5e974287c79ab8d3e0e155d1c79c6d667ec
Berkay Kaya <berkaykayaforbusiness@outlook.com>
2023-12-02 00:31:44 +0300

    Files created and general styles setted.

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Search from '$lib/components/Sidebar/Search.svelte';
+</script>
+
+<section id="sidebar">
+    <Search />
+</section>
+
+<style>
+    #sidebar{
+        height: 100vh;
+        background-color: var(--current-front-color);
+        border-right: 1px solid var(--current-border-color);
+        border-left: 1px solid var(--current-border-color);
+    }
+</style>
---
+++ src/lib/components/Sidebar/Search.svelte
@@ -0,0 +1,24 @@
+<div id="search">
+    <form action="" method="get">
+        <input type="text" name="s" id="s" placeholder="Search" />
+    </form>
+</div>
+
+<style>
+    #search {
+        width:350px;
+    }
+
+    #search form {
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        padding:10px;
+    }
+
+    #search input {
+        height:30px;
+        width: 100%;
+        padding-left:10px;
+    }
+</style>
