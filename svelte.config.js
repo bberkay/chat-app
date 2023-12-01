42ae598720cdc612fe068ba6e2638c231d3dd5bf
Berkay Kaya <berkaykayaforbusiness@outlook.com>
2023-12-01 16:57:45 +0300

    svelte-config-adapter-fixed

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-bun';;
+import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
