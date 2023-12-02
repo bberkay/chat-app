23673bcb8eb5eaea6b96c6853567ffa83c33dc81
Berkay Kaya <berkaykayaforbusiness@outlook.com>
2023-12-02 20:10:45 +0300

    Theme-icon-is-changing-now

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,8 +3,16 @@
     import { page } from '$app/stores';
 
     onMount(() => {
+        /* TODO: Active doesn't update when navigating to a new page */
         document.querySelector('a[href="' + $page.url.pathname + '"]').classList.add('active');
     });
+
+    let isDark = false;
+
+    function changeTheme() {
+        window.document.body.classList.toggle('dark');
+        isDark = !isDark;
+    }
 </script>
 
 <nav>
@@ -14,9 +22,13 @@
     <a href="/profiles">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path></svg>
     </a>
-    <a href = "#" on:click={() => { window.document.body.classList.toggle('dark') }}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M240,96a8,8,0,0,1-8,8H216v16a8,8,0,0,1-16,0V104H184a8,8,0,0,1,0-16h16V72a8,8,0,0,1,16,0V88h16A8,8,0,0,1,240,96ZM144,56h8v8a8,8,0,0,0,16,0V56h8a8,8,0,0,0,0-16h-8V32a8,8,0,0,0-16,0v8h-8a8,8,0,0,0,0,16Zm72.77,97a8,8,0,0,1,1.43,8A96,96,0,1,1,95.07,37.8a8,8,0,0,1,10.6,9.06A88.07,88.07,0,0,0,209.14,150.33,8,8,0,0,1,216.77,153Zm-19.39,14.88c-1.79.09-3.59.14-5.38.14A104.11,104.11,0,0,1,88,64c0-1.79,0-3.59.14-5.38A80,80,0,1,0,197.38,167.86Z"></path></svg>
-    </a>
+    <button on:click={changeTheme}>
+        {#if !isDark}
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M120,40V16a8,8,0,0,1,16,0V40a8,8,0,0,1-16,0Zm72,88a64,64,0,1,1-64-64A64.07,64.07,0,0,1,192,128Zm-16,0a48,48,0,1,0-48,48A48.05,48.05,0,0,0,176,128ZM58.34,69.66A8,8,0,0,0,69.66,58.34l-16-16A8,8,0,0,0,42.34,53.66Zm0,116.68-16,16a8,8,0,0,0,11.32,11.32l16-16a8,8,0,0,0-11.32-11.32ZM192,72a8,8,0,0,0,5.66-2.34l16-16a8,8,0,0,0-11.32-11.32l-16,16A8,8,0,0,0,192,72Zm5.66,114.34a8,8,0,0,0-11.32,11.32l16,16a8,8,0,0,0,11.32-11.32ZM48,128a8,8,0,0,0-8-8H16a8,8,0,0,0,0,16H40A8,8,0,0,0,48,128Zm80,80a8,8,0,0,0-8,8v24a8,8,0,0,0,16,0V216A8,8,0,0,0,128,208Zm112-88H216a8,8,0,0,0,0,16h24a8,8,0,0,0,0-16Z"></path></svg>
+        {:else}
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M240,96a8,8,0,0,1-8,8H216v16a8,8,0,0,1-16,0V104H184a8,8,0,0,1,0-16h16V72a8,8,0,0,1,16,0V88h16A8,8,0,0,1,240,96ZM144,56h8v8a8,8,0,0,0,16,0V56h8a8,8,0,0,0,0-16h-8V32a8,8,0,0,0-16,0v8h-8a8,8,0,0,0,0,16Zm72.77,97a8,8,0,0,1,1.43,8A96,96,0,1,1,95.07,37.8a8,8,0,0,1,10.6,9.06A88.07,88.07,0,0,0,209.14,150.33,8,8,0,0,1,216.77,153Zm-19.39,14.88c-1.79.09-3.59.14-5.38.14A104.11,104.11,0,0,1,88,64c0-1.79,0-3.59.14-5.38A80,80,0,1,0,197.38,167.86Z"></path></svg>
+        {/if}
+    </button>
     <span class = "active"></span>
 </nav>
 
@@ -28,28 +40,30 @@
         flex-direction: column;
     }
 
-    nav a{
+    nav a, nav button{
+        border:none;
         text-align: center;
         border-bottom: 2px solid var(--current-border-color);
         cursor:pointer;
         padding:15px;
+        background-color: transparent;
     }
 
     nav .active{
         background-color: var(--current-front-color);
     }
 
-    nav a svg{
+    nav a svg, nav button svg{
         width: 1.8em;
         height: 1.8em;
         color: darkgrey;
     }
 
-    nav a:hover{
+    nav a:hover, nav button:hover{
         background-color: var(--current-front-color);
     }
 
-    nav a:hover svg{
+    nav a:hover svg, nav button:hover svg{
         color: #fff;
     }
 </style>
