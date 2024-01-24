8acaef0caf97d46921619634691b9f03bd5a388a
Berkay Kaya <berkaykayaforbusiness@outlook.com>
2024-01-25 01:51:07 +0300

    home-page-blockquote

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -4,10 +4,10 @@
         This is a simple chat application with SvelteKit, Bun, and MongoDB. It's designed to demonstrate how these technologies can seamlessly
         integrate to create a real-time, user-friendly chat experience as easily as possible.
     </p>
-    <p>
-        <b>Tip: </b> To testing real-time chat, open this page on another browser/device and sync them with the session ID in the profile section
+    <blockquote>
+        To testing real-time chat, open this page on another browser/device and sync them with the session ID in the profile section
         at the bottom left of the app.
-    </p>
+    </blockquote>
     <i><a href = "https://github.com/bberkay/sveltekit-bun-mongodb" target = "_blank">For more information and source code...</a></i>
 </section>
 
@@ -20,6 +20,18 @@
     #home h1{
         font-size: 3rem;
         margin-bottom: 3rem;
+    }
+
+    #home blockquote{
+        border-left: 3px solid #ccc;
+        padding: 1rem;
+        margin-left: 0;
+        background-color: rgba(70, 69, 69, 0.11);
+        font-style: italic;
+    }
+
+    #home a{
+        color: blue;
     }
 
     @media only screen and (min-width: 768px) {
