5d92303f3fb6432181f0b6898322461f74f4c6c1
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-02 10:19:45 +0300

    Update +page.server.ts

--- app/src/routes/messages/[friendId]/+page.server.ts
+++ app/src/routes/messages/[friendId]/+page.server.ts
@@ -1,24 +1,21 @@
-import { Server } from '$lib/classes/Server';
+import { ChatApiService } from '$lib/classes/ChatApiService';
 import type { User } from '$lib/types';
 import { droid } from '$lib/classes/Droid';
-import { usersStore } from "$lib/stores";
-import { get } from 'svelte/store';
 
 export async function load({ params, cookies }: { params: { friendId: string }, cookies: any }): Promise<{profile: User, friend: User}>
 {
-    // Get the profile from the cookies.
-    const profile = JSON.parse(cookies.get("profile"));
+    let profile = cookies.get("profile");
+    if (profile) profile = JSON.parse(profile);
 
-    // Get the user id from params then find the user from the classes with that id.
     const friendId: string = params.friendId;
     if(friendId === "droid")
         return {profile: profile, friend: droid}
 
-    // If friend is not droid then find the user from the classes with that id.
-    const friend = get(usersStore).find(user => user._id === friendId);
-    if(!friend) throw new Error("Friend not found.");
+    let friends = await ChatApiService.getUsersWithDroid();
+    const friend = friends.find(friend => friend._id === friendId);
+    if(!friend)
+        throw new Error("Friend not found.");
 
-    // Return friend for chatting.
     return {
         profile: profile,
         friend: friend!,
