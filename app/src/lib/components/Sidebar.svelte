e59ecf4d27ab53676d164f8b0802c9320b1e0887
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-01-02 10:18:06 +0300

    Update Sidebar.svelte

--- app/src/lib/components/Sidebar.svelte
+++ app/src/lib/components/Sidebar.svelte
@@ -1,54 +1,42 @@
 <script lang="ts">
-    import type { User } from '$lib/types';
     import { onMount } from "svelte"
-    import { searchResultsStore, messagesStore, profileStore } from '$lib/stores';
+    import { searchResultsStore, messagesStore } from '$lib/stores';
     import Search from '$lib/components/Sidebar/Search.svelte';
     import Profile from '$lib/components/Sidebar/Profile.svelte';
     import FriendCard from '$lib/components/Sidebar/FriendCard.svelte';
     import { get } from 'svelte/store';
-
-    export let profile: User;
-    export let users: User[] = [];
+    import type { Friend, Message } from "$lib/types";
 
     /**
      * Responsive Sidebar
      */
-    let innerWidth = 0;
     onMount(() => {
-        innerWidth = window.innerWidth;
         document.querySelector('#sidebar .loading')?.remove();
-        searchResultsStore.set(users.filter(user => user._id !== profile._id));
+        sortByLastMessage();
     })
 
-    /* Remove hide when the window is resized to a width greater than 768px */
-    $: if (innerWidth > 768) {
-        document.querySelector('#sidebar')?.classList.remove('hide');
-    }
-
     /**
-     * Profile Change
-     * Change users when profile changes.
+     * Sort friends by last message
      */
-    profileStore.subscribe((value) => {
-        searchResultsStore.set(users.filter(user => user._id !== value._id));
-    });
+    function sortByLastMessage() {
+        searchResultsStore.set(get(searchResultsStore).sort((a, b) => {
+            const dateA = a.lastMessage?.sentAt.getTime() ?? -Infinity;
+            const dateB = b.lastMessage?.sentAt.getTime() ?? -Infinity;
+            return dateB - dateA;
+        }));
+    }
 
     /**
      * Messages
      * Sort friends by last message when a new message is received.
      */
-    messagesStore.subscribe((value) => {
-        if(value.length === 0) return;
-        const searchResults = get(searchResultsStore);
-        const lastMessage = value[value.length - 1];
-        const index = searchResults.findIndex(user => user._id === lastMessage.senderId);
-        const temp = searchResults.splice(index, 1);
-        searchResults.unshift(temp[0]);
-        searchResultsStore.set(searchResults);
+    messagesStore.subscribe((messages: Message[]) => {
+        if(messages.length === 0)
+            return;
+
+        sortByLastMessage();
     })
 </script>
-
-<svelte:window bind:innerWidth/>
 
 <section id="sidebar">
     <Search />
@@ -56,10 +44,10 @@
         <span class = "loader"></span>
         <span>Loading...</span>
     </div>
-    {#each $searchResultsStore as user}
-        <FriendCard user="{user}"/>
+    {#each $searchResultsStore as friend}
+        <FriendCard friend="{friend}"/>
     {/each}
-    <Profile user="{profile}"/>
+    <Profile/>
     <span class = "hide"></span>
 </section>
 
@@ -70,10 +58,10 @@
         border-right: 1px solid var(--border-color);
         position:relative;
         display: block;
-    }
 
-    .hide {
-        display:none!important;
-        border-right: 0 !important; /* remove border right from sidebar */
+        &.hide {
+            display:none!important;
+            border-right: 0 !important;
+        }
     }
 </style>
