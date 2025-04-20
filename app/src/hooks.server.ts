520946c141fb3ccf66a81c8252295900f06bd614
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-04-20 21:32:17 +0300

    Update hooks.server.ts

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -3,6 +3,6 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = ({ event, resolve }) => {
     const theme = event.cookies.get("theme") || "system";
 	return resolve(event, {
-        transformPageChunk: ({ html }) => html.replace('%data-theme%', theme)
+        transformPageChunk: ({ html }) => html.replace('%theme%', theme)
 	});
 };
